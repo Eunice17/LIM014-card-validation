3adb4ae693ee90d1dfa1141e730c0ce1de39d62b
Eunice Fiorella <euniceancajima@gmail.com>
2021-01-15 16:49:16 -0500

    Avance de maquetacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
+const inputs=document.querySelectorAll(".inputs");
+const label=document.querySelectorAll(".label");
+
+
+eventoLabels();
+function eventoLabels(){
+    for(let i=0;i<inputs.length;i++){
+        
+        inputs[i].addEventListener('focus',function(){
+            label[i].classList.add('focus');
+        });
+            inputs[i].addEventListener('blur',function(){
+                if(inputs[i].value==""){
+                    label[i].classList.remove('focus');
+                }
+            });
+    }
+}
+
 console.log(validator);
