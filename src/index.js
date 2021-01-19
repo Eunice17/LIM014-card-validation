7e14ebb2764f71d51edb088deaba2b83a76a5da7
Eunice Fiorella <euniceancajima@gmail.com>
2021-01-18 19:03:19 -0500

    Muestra validacion de numero de tarjeta de credito

--- src/index.js
+++ src/index.js
@@ -2,21 +2,84 @@ import validator from './validator.js';
 
 const inputs=document.querySelectorAll(".inputs");
 const label=document.querySelectorAll(".label");
-
+const validar=document.getElementById('validar');
+const name=document.getElementById('name');
+const numero=document.getElementById('numero');
+const mensaje=document.getElementById('mensaje');
 
 eventoLabels();
+eventosInput();
+
 function eventoLabels(){
     for(let i=0;i<inputs.length;i++){
         
         inputs[i].addEventListener('focus',function(){
+            mensaje.textContent='Validar número';
             label[i].classList.add('focus');
+            label[i].classList.remove('invalida');
+            label[i].classList.remove('valida');
+            mensaje.classList.remove('valida');
+            mensaje.classList.remove('invalida');
         });
             inputs[i].addEventListener('blur',function(){
                 if(inputs[i].value==""){
+                    mensaje.textContent='Validar número';
                     label[i].classList.remove('focus');
+                    label[i].classList.remove('invalida');
+                   
                 }
             });
     }
 }
 
-console.log(validator);
+function eventosInput(){
+    inputs[0].addEventListener('keyup',(e)=>{
+        let numero=e.target.value;
+
+        let esto=numero.replace(/\s/g,'')
+        .replace(/\D/g,'').replace(/([0-9]{4})/g,'$1 ').trim();
+        inputs[0].value=esto;
+    });
+
+    inputs[1].addEventListener('keyup',(e)=>{
+        let nombre=e.target.value;
+        name.textContent=nombre;
+    });
+}
+
+validar.addEventListener('click',()=>{
+        let num=inputs[0].value;
+        if(num!=''){
+            let mostrarNum=num.replace(/\s/g,'');
+            let bandera=validator.isValid(mostrarNum);
+            let michi=validator.miskify(mostrarNum);
+
+            resultados(bandera,michi);
+
+        }else{
+            alert('Ingrese Número de tarjeta');
+        }
+});
+
+function resultados(bandera,michi){
+    numero.textContent=michi;
+    if(bandera==true){
+        mensaje.textContent='Número de tarjeta válida';
+        mensaje.classList.remove('invalida');
+        label[0].classList.remove('invalida');
+        label[1].classList.remove('invalida');
+        mensaje.classList.add('valida');
+        label[0].classList.add('valida');
+        label[1].classList.add('valida');
+
+    }else{
+        mensaje.textContent='Número de tarjeta inválida';
+        mensaje.classList.remove('valida');
+        label[0].classList.remove('valida');
+        label[1].classList.remove('valida');
+        mensaje.classList.add('invalida');
+        label[0].classList.add('invalida');
+        label[1].classList.add('invalida');
+    }
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
-};
+  miskify:(nIngresado)=>{
 
+    return mostrarNumero(nIngresado);
+    
+  },
+  isValid:(nIngresado)=>{
+
+    return validacion(nIngresado);
+  }
+};
+function mostrarNumero(n){
+  let cadena="",char='#';
+  if(n.length>4){
+    let cuatro=n.slice(n.length-4);
+
+    cadena=char.repeat(n.length-4)+cuatro;
+  }else{
+    cadena=n;
+  }
+  return cadena;
+}
+
+function validacion(num){
+  let respuesta=false;
+  let residuo=0;
+  let sumPares=0,sumaTotal=0;
+  let sumImpares=0;
+  console.log(num);
+  if(num.length>=14){
+    for(let i=num.length-2;i>=0;i=i-2){
+      let unidades=num[i]*2;
+      residuo=0;
+      while(unidades>0){
+        residuo+=unidades%10;
+        unidades=Math.floor(unidades/10);
+      }
+      sumPares+=residuo;
+    }
+    for(let j=num.length-1;j>=0;j=j-2){
+      sumImpares+=parseInt(num[j]);
+    }
+    sumaTotal=parseInt(sumPares)+parseInt(sumImpares);
+    console.log(sumaTotal);
+      if((sumaTotal%10)==0){
+       respuesta=true;
+     }
+  }
+  return respuesta;
+}
 export default validator;
